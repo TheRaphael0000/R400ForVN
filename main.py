a1b58a9c1cf8faa60132e9dead89205790203809
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-08-13 11:10:58 +0200

    add a count for the inputs

--- main.py
+++ main.py
@@ -3,18 +3,25 @@
 import pynput.keyboard as kb
 import pynput.mouse as mo
 
+count = 0
+
+def printcount(*arg, **argv):
+    global count
+    print("[" + str(count).rjust(5, "0") + "]", *arg, **argv)
+    count += 1
+
 def main():
-    print("R400ForVN started !")
+    printcount("R400ForVN started !")
     keyboard_controller = kb.Controller()
     mouse_controller = mo.Controller()
 
     def on_press(key):
         if key == kb.Key.page_up:
-            print("Scoll Up")
+            printcount("Scoll Up")
             mouse_controller.scroll(0, 1)
 
         if key == kb.Key.page_down:
-            print("Scoll Down")
+            printcount("Scoll Down")
             mouse_controller.scroll(0, -1)
 
         if str(key) == "'.'":
@@ -23,7 +30,8 @@ def main():
     with kb.Listener(on_press=on_press) as listener:
         listener.join()
 
-    print("R400ForVN stopped !")
+    printcount("R400ForVN stopped !")
+
 
 if __name__ == '__main__':
     main()
