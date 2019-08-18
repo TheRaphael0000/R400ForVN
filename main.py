b2f6988e8d0f340e9b81d56dc3e23c0effa456ef
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-08-18 20:43:22 +0200

    add a reset count button

--- main.py
+++ main.py
@@ -5,32 +5,42 @@ import pynput.mouse as mo
 
 count = 0
 
-def printcount(*arg, **argv):
+def prt_count(*arg, **argv):
     global count
     print("[" + str(count).rjust(5, "0") + "]", *arg, **argv)
-    count += 1
+
+def inc_count(i):
+    global count
+    count += i
+
+def rst_count():
+    global count
+    count = 0
 
 def main():
-    printcount("R400ForVN started !")
+    prt_count("R400ForVN started !")
     keyboard_controller = kb.Controller()
     mouse_controller = mo.Controller()
 
     def on_press(key):
         if key == kb.Key.page_up:
-            printcount("Scoll Up")
             mouse_controller.scroll(0, 1)
-
-        if key == kb.Key.page_down:
-            printcount("Scoll Down")
+            inc_count(-1)
+            prt_count("Scoll Up")
+        elif key == kb.Key.page_down:
             mouse_controller.scroll(0, -1)
-
-        if str(key) == "'.'":
+            inc_count(1)
+            prt_count("Scoll Down")
+        elif key == kb.Key.f5 or key == kb.Key.esc:
+            rst_count()
+            prt_count("Count reseted !")
+        elif str(key) == "'.'":
             return False
 
     with kb.Listener(on_press=on_press) as listener:
         listener.join()
 
-    printcount("R400ForVN stopped !")
+    prt_count("R400ForVN stopped !")
 
 
 if __name__ == '__main__':
